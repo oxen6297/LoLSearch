e22f90877d2b2eb54a0ed1145617596db090395c
oxen6297 <oxen6297@naver.com>
2024-06-24 20:09:13 +0900

    feat: create dataStore di

---
+++ data/src/main/java/com/sb/park/data/di/DataStoreModule.kt
@@ -0,0 +1,26 @@
+package com.sb.park.data.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val DATASTORE_NAME = "VERSION_DATASTORE"
+
+    private val Context.myDataStore by preferencesDataStore(DATASTORE_NAME)
+
+    @Provides
+    @Singleton
+    fun provideDataStore(@ApplicationContext context: Context): DataStore<Preferences> =
+        context.myDataStore
+}

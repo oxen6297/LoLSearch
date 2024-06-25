067301ba00868269dd01b725a78b29393780349b
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-06-25 15:57:15 +0900

    feat: create screen

---
+++ app/src/main/java/com/sb/park/lol/screen/DictionaryScreen.kt
@@ -0,0 +1,8 @@
+package com.sb.park.lol.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DictionaryScreen() {
+    //TODO DictionaryScreen
+}
---
+++ app/src/main/java/com/sb/park/lol/screen/SearchScreen.kt
@@ -0,0 +1,8 @@
+package com.sb.park.lol.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SearchScreen() {
+    //TODO SearchScreen
+}

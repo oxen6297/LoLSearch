559707b577942c7df8804b81e1352c7871dde587
oxen6297 <oxen6297@naver.com>
2024-06-28 01:14:18 +0900

    feat: create ShimmerLayout

--- app/src/main/java/com/sb/park/lol/screen/DictionaryScreen.kt
+++ app/src/main/java/com/sb/park/lol/screen/DictionaryScreen.kt
@@ -1,8 +1,80 @@
 package com.sb.park.lol.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color.Companion.LightGray
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.valentinilk.shimmer.shimmer
 
 @Composable
 fun DictionaryScreen() {
-    //TODO DictionaryScreen
+    ChampionShimmer()
+}
+
+@Composable
+fun ChampionShimmer(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(20.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(3) {
+            ItemShimmer(modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+fun ItemShimmer(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        repeat(3) {
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(0.7f)
+                    .shimmer()
+                    .background(
+                        color = LightGray,
+                        shape = RoundedCornerShape(4.dp),
+                    )
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(30.dp)
+                    .shimmer()
+                    .background(
+                        color = LightGray,
+                        shape = RoundedCornerShape(4.dp),
+                    )
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ShimmerPreview() {
+    ChampionShimmer()
 }

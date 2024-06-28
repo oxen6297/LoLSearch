4cd2bbfa47b9b8e7beaa5d7c4aa29b8cddca9739
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-06-28 15:34:16 +0900

    feat: error snackBar 구현

--- app/src/main/java/com/sb/park/lol/MainActivity.kt
+++ app/src/main/java/com/sb/park/lol/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.NavHost
@@ -21,6 +24,8 @@ import com.sb.park.lol.screen.SearchScreen
 import com.sb.park.lol.screen.SettingScreen
 import com.sb.park.lol.viewmodels.SplashViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
+import java.net.UnknownHostException
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -37,6 +42,19 @@ class MainActivity : ComponentActivity() {
                 }
 
                 val navController = rememberNavController()
+                val snackBarHostState = remember { SnackbarHostState() }
+                val coroutineScope = rememberCoroutineScope()
+                val onShowSnackBar: (throwable: Throwable?) -> Unit = { throwable ->
+                    coroutineScope.launch {
+                        snackBarHostState.showSnackbar(
+                            when (throwable) {
+                                is UnknownHostException -> getString(R.string.snack_bar_error_network)
+                                else -> getString(R.string.snack_bar_error_unknown)
+                            }
+                        )
+                    }
+                }
+
                 Scaffold(
                     bottomBar = { BottomNavigation(navController = navController) }
                 ) {
@@ -46,7 +64,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.padding(it)
                     ) {
                         composable(BottomNavItem.Dictionary.route) {
-                            DictionaryScreen()
+                            DictionaryScreen(onShowSnackBar, navController)
                         }
                         composable(BottomNavItem.Search.route) {
                             SearchScreen()

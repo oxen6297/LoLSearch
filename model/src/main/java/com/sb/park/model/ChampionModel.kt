158f85fbddaec72a8cfdbae6c59995fd0e169701
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-06-28 15:22:27 +0900

    refactor: delete annotation

--- model/src/main/java/com/sb/park/model/ChampionModel.kt
+++ model/src/main/java/com/sb/park/model/ChampionModel.kt
@@ -2,10 +2,8 @@ package com.sb.park.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.squareup.moshi.JsonClass
 
 @Entity
-@JsonClass(generateAdapter = true)
 data class ChampionModel(
     @PrimaryKey val id: String,
     val name: String

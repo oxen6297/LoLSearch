7fcfad352a6dd88388f581c344d7cc96e6b62312
oxen6297 <oxen6297@naver.com>
2024-06-30 01:59:41 +0900

    feat: create topBar

---
+++ designsystem/src/main/java/com/sb/park/designsystem/widget/TopBar.kt
@@ -0,0 +1,22 @@
+package com.sb.park.designsystem.widget
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TopBar(title: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(50.dp),
+        contentAlignment = Alignment.CenterStart
+    ) {
+        Text(text = title)
+    }
+}

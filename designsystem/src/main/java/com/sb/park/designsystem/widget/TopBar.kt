c29fc572f593f5503289880fcd18b37e85bec364
oxen6297 <oxen6297@naver.com>
2024-07-01 23:33:03 +0900

    refactor: change color / set padding

--- designsystem/src/main/java/com/sb/park/designsystem/widget/TopBar.kt
+++ designsystem/src/main/java/com/sb/park/designsystem/widget/TopBar.kt
@@ -1,8 +1,11 @@
 package com.sb.park.designsystem.widget
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,7 +18,9 @@ fun TopBar(title: String, style: TextStyle, modifier: Modifier = Modifier) {
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(50.dp),
+            .height(50.dp)
+            .background(MaterialTheme.colorScheme.primary)
+            .padding(start = 20.dp, end = 20.dp),
         contentAlignment = Alignment.CenterStart
     ) {
         Text(text = title, style = style)

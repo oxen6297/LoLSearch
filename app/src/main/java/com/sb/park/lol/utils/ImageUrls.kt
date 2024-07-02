40af46c34b8b028da6e82994b9ab288fd63ca76f
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-07-02 14:40:28 +0900

    refactor: 네이밍 변경

--- app/src/main/java/com/sb/park/lol/utils/ImageUrls.kt
+++ app/src/main/java/com/sb/park/lol/utils/ImageUrls.kt
@@ -1,5 +1,4 @@
 package com.sb.park.lol.utils
 
-internal val mainImage: (String) -> String = { id ->
+internal fun mainImage(id: String): String =
     "http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${id}_0.jpg"
-}

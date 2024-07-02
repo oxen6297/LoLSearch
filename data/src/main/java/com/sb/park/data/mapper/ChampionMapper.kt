91273f6fedd2795a4335e5731d0f2c29771dd861
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-07-02 14:40:25 +0900

    refactor: 네이밍 변경

--- data/src/main/java/com/sb/park/data/mapper/ChampionMapper.kt
+++ data/src/main/java/com/sb/park/data/mapper/ChampionMapper.kt
@@ -3,7 +3,7 @@ package com.sb.park.data.mapper
 import com.sb.park.data.model.datadragon.ChampionResponse
 import com.sb.park.model.ChampionModel
 
-fun ChampionResponse.toData(): ChampionModel = ChampionModel(
+fun ChampionResponse.toModel(): ChampionModel = ChampionModel(
     id = this.id,
     name = this.name
 )

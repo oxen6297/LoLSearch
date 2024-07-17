916693e5382367611686ce88e073c703f9d48097
sbpark <112065694+oxen6297@users.noreply.github.com>
2024-07-17 12:57:49 +0900

    refactor: add Stable / Immutable

--- designsystem/src/main/java/com/sb/park/designsystem/UiState.kt
+++ designsystem/src/main/java/com/sb/park/designsystem/UiState.kt
@@ -1,20 +1,25 @@
 package com.sb.park.designsystem
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.Stable
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onStart
 
+@Stable
 sealed interface UiState<out T> {
+    @Immutable
     data object Loading : UiState<Nothing>
+    @Immutable
     data class Success<out T>(val data: T) : UiState<T>
+    @Immutable
     data class Error(val e: Throwable) : UiState<Nothing>
 }
 
 fun <T> UiState<T>.onSuccess(): T = (this as UiState.Success).data
 
 fun <T> UiState<T>.onError(): Throwable  = (this as UiState.Error).e
-
 
 inline fun <T> safeFlow(
     crossinline service: suspend () -> T
